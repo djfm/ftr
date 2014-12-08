53bce378df360361621d0f0a34a536ed52953cd8
fram <fm.de.jouvencel@gmail.com>
2014-12-08 16:54:21 +0100

    // formatting

--- src/TestPlan/TestPlanInterface.php
+++ src/TestPlan/TestPlanInterface.php
@@ -7,9 +7,9 @@ use djfm\ftr\Exception\TestPlanException;
 interface TestPlanInterface
 {
 	/**
-	 * 
 	 * @return array [seq, seq, seq] where seq is an array of Execution Plans
 	 */
 	public function getExecutionPlans();
+		
 	public function getTestsCount();
 }
